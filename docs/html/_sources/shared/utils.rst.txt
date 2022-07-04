a25617145aef666ef01e60375b8a1c75a6e4e95c
Stijn Kamp <s.l.kamp@student.tue.nl>
2022-07-04 12:39:55 +0100

    Updated docs

---
+++ docs/html/_sources/shared/utils.rst.txt
@@ -0,0 +1,8 @@
+Utils helpers
+=============
+Utils contains all the common helpers that every service can use. It consist of some general functions like getting a dict obj from a .json file. 
+
+.. automodule:: utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- web/helpers.py
+++ web/helpers.py
@@ -4,6 +4,15 @@ from flask.json import dumps as json_dumps
 
 
 def get_input(columns=None, request_data=None):
+    """Retrieve speficied columns from request data. If the column is not available it wont be added to the returned dictionary.
+
+    Args:
+        columns (list, optional): Column keys you want to retrieve. Defaults to all columns.
+        request_data (dict, optional): The entire request data. Defaults to request data from flask.
+
+    Returns:
+        dict: The resulting dictionary with only the needed columns that are not emtpy
+    """
     if request_data == None:
         request_data = request.json 
     data = {}
@@ -16,15 +25,43 @@ def get_input(columns=None, request_data=None):
 
 
 def parse_model(item):
+    """Parse a SQLAlchemy model to a dictionary
+
+    Args:
+        item (SQLAlechmyModel): The SQLAlchemyModel
+
+    Returns:
+        dict: The represented dict.
+    """
     return json.loads(json_dumps(item))
 
 def add_relationship(item, relationship):
+    """Add specified raltionship to SQLAlchemy model and return the dictionary
+
+    Args:
+        item (SQLAlechmyModel): The model where a relationship needs to be added
+        relationship (string): The relationship
+
+    Returns:
+        dict: The model including the added relationship.
+    """
     relation = getattr(item, relationship)
     item = parse_model(item)
     item[relationship] = relation
     return item
 
 def get_or_create(session, model, query, values = None):
+    """Get model or create one if it does not exists according to the query
+
+    Args:
+        session (SQLAlchemySession): The current database session
+        model (SQLAlchemyModel): The model which needs to be retrieved/created
+        query (dict): The query to which to retrieve the model if exists
+        values (dict, optional): The values when creating the model. Defaults to the query dictionary.
+
+    Returns:
+        SQLAlchemyModel: The retrieved or created model. 
+    """
     if values == None:
         values = query
     instance = session.query(model).filter_by(**query).first()
@@ -36,6 +73,17 @@ def get_or_create(session, model, query, values = None):
         session.commit()
         return instance
 def update_or_create(session, model, query, values = None):
+    """Update model or create one if it does not exists according to the query
+
+    Args:
+        session (SQLAlchemySession): The current database session
+        model (SQLAlchemyModel): The model which needs to be retrieved/created
+        query (dict): The query to which to retrieve the model if exists
+        values (dict, optional): The values when creating the model. Defaults to the query dictionary.
+
+    Returns:
+        SQLAlchemyModel: The retrieved or created model. 
+    """
     if values == None:
         values = query
     instance = session.query(model).filter_by(**query).first()
